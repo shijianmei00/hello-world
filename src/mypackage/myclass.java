ec21e772bb03853d555240c9098aa841d35797f7
admin <admin@admins-MacBook-Pro.local>
2017-07-01 00:19:08 +0800

    Initial commit

---
+++ src/mypackage/myclass.java
@@ -0,0 +1,11 @@
+package mypackage;
+
+/**
+ * Created by admin on 30/6/17.
+ */
+public class myclass
+{
+    public static void main(String[] args){
+        System.out.print("hello world");
+    }
+}
